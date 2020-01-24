a5b785f59c9e1e80c3ce0241b3b4611b0b0e3d42
norimasa-itabashi <norimasa-itabashi@navitime.co.jp>
2020-01-24 14:15:27 +0900

    create search tab ui

--- lib/view/search.dart
+++ lib/view/search.dart
@@ -1,22 +1,55 @@
 import 'package:flutter/material.dart';
 
 class SearchPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text('ここに検索ボックスを置く', style: Theme
-                    .of(context)
-                    .textTheme
-                    .headline),
-              ],
-            )
-        ),
+      body: Center(
+          child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'タイトル',
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: '著者',
+              ),
+            ),
+          ),
+          Spacer(),
+          Row(
+            children: [
+              Expanded(
+                child: FlatButton(
+                  padding: EdgeInsets.all(16.0),
+                  onPressed: () {
+                    /**
+                         *
+                         */
+                  },
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                  child: Text(
+                    '検索する',
+                    style: TextStyle(fontSize: 16.0),
+                  ),
+                ),
+              )
+            ],
+          )
+        ],
+      )),
     );
   }
 }
